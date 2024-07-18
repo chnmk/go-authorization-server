4f7fa75317a70a16c33e3fb5e952b387caadc10a
chnmk <a2molchanov@ya.ru>
2024-07-18 11:44:53 +0300

    feat: add singup handler placeholder

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ import (
 )
 
 func mainHandle(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("Incoming request!")
-	res.Write([]byte("Hello, World!"))
+	fmt.Println("Incoming request to: main")
+	res.Write([]byte("Hello from main!"))
+}
+
+func signUpPost(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("Incoming request to: signup")
+	if req.Method == http.MethodPost {
+		fmt.Println("Got post request")
+	}
+	res.Write([]byte("Hello from signup!"))
 }
 
 func main() {
@@ -15,6 +23,7 @@ func main() {
 
 	fmt.Println("Server starting...")
 	http.HandleFunc(`/`, mainHandle)
+	http.HandleFunc(`/signup`, signUpPost)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
