b9dd63f77ac291db5374320568fa2fca236fcc78
chnmk <a2molchanov@ya.ru>
2024-07-23 13:38:18 +0300

    feat: allow jwt post requests

--- main.go
+++ main.go
@@ -16,7 +16,19 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/signup", handler.SignupHandler)
 
-	handler := cors.Default().Handler(mux)
+	//handlerDefault := cors.Default().Handler(mux)
+	handler := cors.New(
+		cors.Options{
+			AllowedMethods:     []string{"POST", "OPTIONS"},
+			AllowedOrigins:     []string{"*"},
+			AllowedHeaders:     []string{"Bearer", "Bearer ", "authorization", "content-type", "authorization,content-type"},
+			AllowCredentials:   true,
+			OptionsPassthrough: true,
+			// Enable Debugging for testing, consider disabling in production
+			Debug: true,
+		},
+	).Handler(mux)
+
 	err := http.ListenAndServe(":8080", handler)
 	if err != nil {
 		panic(err)
--- transport/rest/signup.go
+++ transport/rest/signup.go
@@ -1,37 +1,39 @@
 package handler
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"net/http"
-
-	service "github.com/chnmk/sample-authorization-backend/services"
+	"strings"
 )
 
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Incoming request to: signup")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
 
 	if r.Method == http.MethodPost {
 		fmt.Println("Got post request")
 
-		var user service.User
-		var buf bytes.Buffer
+		// var user service.User
+		header := r.Header.Get("Authorization")
 
-		_, err := buf.ReadFrom(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+		if header != "" {
+			token := strings.Split(header, " ")
+			if len(token) == 2 {
+				// ...
+				fmt.Println(token[0])
+				fmt.Println(token[1])
+
+				w.Write([]byte("success"))
+				return
+			}
 		}
+		http.Error(w, "Header error", http.StatusBadRequest)
+		return
+	}
 
-		if err = json.Unmarshal(buf.Bytes(), &user); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		fmt.Println("Got username: ", user.Username)
-
-		// TODO: change response writer
-		w.Write([]byte("Hello from signup!"))
+	if r.Method != http.MethodOptions {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 }
