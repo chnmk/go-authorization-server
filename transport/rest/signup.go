e145870002060ca60089c9d89f17b975274708a4
chnmk <a2molchanov@ya.ru>
2024-07-26 12:41:24 +0300

    feat: read request body in signup handler

--- transport/rest/signup.go
+++ transport/rest/signup.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 )
@@ -9,7 +10,7 @@ import (
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Incoming request to: signup")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "authorization, content-type")
 
 	if r.Method == http.MethodPost {
 		fmt.Println("Got post request")
@@ -17,6 +18,7 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 		// var user service.User
 		header := r.Header.Get("Authorization")
 
+		// Read authorization token from request header
 		if header != "" {
 			token := strings.Split(header, " ")
 			if len(token) == 2 {
@@ -25,15 +27,32 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 				fmt.Println(token[1])
 
 				w.Write([]byte("success"))
+			} else {
+				http.Error(w, "Invalid header", http.StatusBadRequest)
 				return
 			}
+		} else {
+			http.Error(w, "Invalid header", http.StatusBadRequest)
+			return
 		}
-		http.Error(w, "Header error", http.StatusBadRequest)
+
+		// Read username and other data from rqeuest body
+		body, err := io.ReadAll(r.Body)
+		r.Body.Close()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+		fmt.Println(string(body))
 		return
 	}
 
+	// Handle preflight requests
 	if r.Method != http.MethodOptions {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	} else {
+		w.Write([]byte("success"))
 		return
 	}
 }
